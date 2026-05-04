24865ad758dc0c8fdb51f080ce38ba224921212f
Kashish Girdhar <kashish1767.be22@chitkara.edu.in>
2026-05-04 21:25:12 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-production-50f4.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
